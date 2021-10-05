bb9900a842a5ec2e134784dce532711ffe9214bd
NikDy <nikita.dyrkov@gmail.com>
2021-10-06 00:22:29 +0300

    Update PlayerContoller
    Start using character controller instead of move transform

--- GameClient/Assets/Scripts/PlayerController.cs
+++ GameClient/Assets/Scripts/PlayerController.cs
@@ -2,28 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
     const string HORIZONTAL = "Horizontal";
     const string VERTICAL = "Vertical";
+    const string OBSTRUCTION = "Obstruction";
 
     private Plane aimPlane;
+    private Rigidbody ownRigidbody;
 
-    public float moveSpeed = 1.0f;
+    private CharacterController ownController;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float gravityValue = -9.81f;
+    public float playerSpeed = 2.0f;
+    public float punchForce = 10f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         aimPlane = new Plane(gameObject.transform.up, gameObject.transform.forward);
+        ownRigidbody = gameObject.GetComponent<Rigidbody>();
+        ownController = gameObject.GetComponent<CharacterController>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * -Input.GetAxis(VERTICAL) +
-                                       Vector3.right * Time.deltaTime * moveSpeed * -Input.GetAxis(HORIZONTAL),
-                                       Space.World);
+
+        groundedPlayer = ownController.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        else
+        {
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            ownController.Move(playerVelocity * Time.deltaTime);
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        ownController.Move(move * Time.deltaTime * -playerSpeed);
+
+       if (move != Vector3.zero)
+       {
+           gameObject.transform.forward = move;
+       }
+
+        
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float enter = 0.0f;
@@ -35,6 +65,16 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //private void OnControllerColliderHit(ControllerColliderHit hit)
+    //{
+    //    Debug.Log(hit.gameObject.name.ToString());
+    //    if(hit.gameObject.CompareTag(OBSTRUCTION))
+    //    {
+    //        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+    //        Vector3 punchVelocity = pushDir * -1 * punchForce;
+    //        ownController.SimpleMove(punchVelocity * Time.deltaTime);
+    //    }
+    //}
 
     private void UpdateAnitmatorFlags()
     {
